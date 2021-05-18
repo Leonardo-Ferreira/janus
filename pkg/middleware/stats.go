f59b8b721426a0a1ee97d31230504721bf91649f
Leonardo-Ferreira <index.xp@gmail.com>
2021-05-17 21:38:53 -0300

    Fixing broken test

--- pkg/middleware/stats.go
+++ pkg/middleware/stats.go
@@ -39,6 +39,7 @@ func (m *Stats) Handler(handler http.Handler) http.Handler {
 			log.WithField("path", r.URL.Path).Warn("Unknown endpoint requested")
 			r.URL.Path = notFoundPath
 		}
+		m.statsClient.TrackRequest(r, t, success)
 
 		m.statsClient.SetHTTPRequestSection(statsSectionRoundTrip).
 			TrackRequest(r, t, success).
